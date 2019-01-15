d057be772b7c506d0529a089ef9a5d269d390b57
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-15 17:41:28 +0300

    add pom.xml file (feature #91)

---
+++ src/main/java/com/kudoji/kman/kmanweb/KmanWebApplication.java
@@ -0,0 +1,14 @@
+package com.kudoji.kman.kmanweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmanWebApplication.class, args);
+	}
+
+}
+
