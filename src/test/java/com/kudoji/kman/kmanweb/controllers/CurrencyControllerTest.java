92424a624de83ba2794ef1b42b0c7198f40da8f1
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-21 16:31:22 +0300

    currency controller tests updated (feature #94)

--- src/test/java/com/kudoji/kman/kmanweb/controllers/CurrencyControllerTest.java
+++ src/test/java/com/kudoji/kman/kmanweb/controllers/CurrencyControllerTest.java
@@ -8,17 +8,15 @@ import com.kudoji.kman.kmanweb.repositories.CurrencyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -71,5 +69,31 @@ public class CurrencyControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("currency/list"))
                 .andExpect(model().attribute("currencies", currencies));
+    }
+
+    @Test
+    public void testEditCurrencyFormWithExistedCurrency() throws Exception{
+        Currency currency = new Currency("American dollar");
+        currency.setId(1);
+        currency.setCode("USD");
+        currency.setRate(1.0f);
+        currency.setStartsWithCode(true);
+
+        when(currencyRepository.findById(1)).thenReturn(Optional.of(currency));
+
+        mockMvc.perform(get("/currencies/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("currency/new"))
+                .andExpect(model().attribute("currency", currency))
+                .andExpect(request().attribute("currencyId", 1));
+    }
+
+    @Test
+    public void testEditCurrencyFormWithNotExistedCurrency() throws Exception{
+        when(currencyRepository.findById(1)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/currencies/edit/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().stringValues("Location", "/currencies/"));
     }
 }
